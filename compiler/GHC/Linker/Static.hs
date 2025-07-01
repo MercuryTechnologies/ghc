9d83732768a756db7726437b767dcf60c197f881
Ian-Woo Kim <ianwookim@gmail.com>
2025-07-01 11:07:39 -0700

    Raise headerpad from 8000 to 16000

--- compiler/GHC/Linker/Static.hs
+++ compiler/GHC/Linker/Static.hs
@@ -266,7 +266,7 @@ linkBinary' staticLink logger tmpfs dflags unit_env o_files dep_units = do
                           --  libraries during runInjectRpaths phase.
                           --
                           --  See Note [Dynamic linking on macOS].
-                          then [ "-Wl,-dead_strip_dylibs", "-Wl,-headerpad,8000" ]
+                          then [ "-Wl,-dead_strip_dylibs", "-Wl,-headerpad,16000" ]
                           else [])
                     ))
 
